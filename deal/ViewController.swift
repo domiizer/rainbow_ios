565f71ac5c30f0bde348985724d999364ebdb441
DEV <dev@r2d2s-imac.local>
2019-11-15 15:42:14 +0700

    Initial Commit

---
+++ deal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  deal
+//
+//  Created by DEV on 11/15/19.
+//  Copyright © 2019 DEV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
